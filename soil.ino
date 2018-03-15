389b3b3b37ce10c3133d4999576dd46a89ca70fc
humback <humback@gmail.com>
2018-03-15 09:20:11 +0100

    Update soil.ino

--- soil.ino
+++ soil.ino
@@ -5,7 +5,6 @@
 String apiKey = "RV05BQ88MEE3MXW6";
 const char* ssid = "humback";
 const char* password = "Hubert1978";
-
 const char* server = "api.thingspeak.com";
 
 int analogPin = A0;     
@@ -17,20 +16,15 @@ WiFiClient client;
 
 void setup() {
 Serial.begin(115200);
-
-
 WiFi.begin(ssid, password);
-
-Serial.println();
-Serial.println();
 Serial.print("Connecting to ");
 Serial.println(ssid);
 
 WiFi.begin(ssid, password);
 
 while (WiFi.status() != WL_CONNECTED) {
-delay(500);
-Serial.print(".");
+  delay(500);
+  Serial.print(".");
 }
 Serial.println("");
 Serial.println("WiFi connected");
@@ -39,13 +33,11 @@ Serial.println("WiFi connected");
 
 void loop() {
 
-
-
 if (client.connect(server,80)) { // "184.106.153.149" or api.thingspeak.com
 
 val = analogRead(analogPin);
-String postStr = apiKey;
 
+String postStr = apiKey;
 postStr +="&field2=";
 postStr += String(val);
 postStr += "\r\n\r\n";
